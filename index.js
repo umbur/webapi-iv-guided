29c8f89cdf4f4086d3b37ae1c32455f491997eb6
umbur <work1415@gmail.com>
2019-02-21 11:22:27 -0700

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
  
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
+});
+
+server.post('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
+});
 
 const port = process.env.PORT || 5000
 server.listen(port, () => {
